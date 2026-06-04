4486d1b809e95c35ddda122224c2d6bebc8f8850
gideb <gideblarbi@gmail.com>
2026-06-04 19:58:37 +0000

    done

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
-//export const API_URL = import.meta.env.VITE_API_URL;
+//export const BASE_URL = "http://localhost:8000";
+export const API_URL = import.meta.env.VITE_API_URL;
 
 // utils/apiPaths.js
 export const API_PATHS = {
