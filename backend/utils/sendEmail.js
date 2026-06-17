4bfcf1b66a576a574d9c993356927bbc4371abb0
gideb <gideblarbi@gmail.com>
2026-06-17 00:02:10 +0000

    testing resend

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,4 +1,4 @@
-const nodemailer = require("nodemailer");
+/* const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, subject, html) => {
   const transporter = nodemailer.createTransport({
@@ -18,4 +18,27 @@ const sendEmail = async (email, subject, html) => {
 };
 
 module.exports = sendEmail;
- 
+ */
+
+//RESEND
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const sendEmail = async (email, subject, html) => {
+  try {
+    const data = await resend.emails.send({
+      from: process.env.FROM_EMAIL,
+      to: email,
+      subject,
+      html,
+    });
+
+    console.log("Email sent:", data);
+  } catch (error) {
+    console.error("Resend error:", error);
+    throw error;
+  }
+};
+
+module.exports = sendEmail;
